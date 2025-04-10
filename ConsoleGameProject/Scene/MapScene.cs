6e0ad6ed4e9b18f1df497e0a016e57771572d54e
munsung143 <akunta3475@gmail.com>
2025-04-10 14:42:26 +0900

    맵 씬 클래스 수정
    맵 씬의 공통되는 함수들을 부모 클래스로 옮겼습니다.

--- ConsoleGameProject/Scene/MapScene.cs
+++ ConsoleGameProject/Scene/MapScene.cs
@@ -12,4 +12,80 @@ public abstract class MapScene : Scene
     protected PlayerObject player;
     protected MapObject pickedUpObject;
     public abstract void PortalInit();
+
+    // string[] map을 그대로 출력하는 것이 아닌, string[] map 을 바탕으로 리스트에 추가된
+    // 맵 오브젝트 인스턴스들의 멤버인 좌표, 심볼을 참조하여 출력합니다.
+    public override void Render()
+    {
+        Console.Clear();
+        Game.Me.PrintInfo();
+        foreach (MapObject obj in mapObjects)
+        {
+            Console.SetCursorPosition(obj.PosX, obj.PosY);
+            Console.Write(obj.Symbol);
+        }
+        Console.SetCursorPosition(player.PosX, player.PosY);
+        Console.Write(player.Symbol);
+        Console.SetCursorPosition(0, map.Length);
+    }
+
+    public override void Input()
+    {
+        key = Console.ReadKey(true).Key;
+    }
+
+    public override void Update()
+    {
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                player.DirectionX = 0;
+                player.DirectionY = -1;
+                break;
+            case ConsoleKey.DownArrow:
+                player.DirectionX = 0;
+                player.DirectionY = 1;
+                break;
+            case ConsoleKey.LeftArrow:
+                player.DirectionX = -1;
+                player.DirectionY = 0;
+                break;
+            case ConsoleKey.RightArrow:
+                player.DirectionX = 1;
+                player.DirectionY = 0;
+                break;
+            case ConsoleKey.Enter:
+                // 플레이어와 겹친 오브젝트를 불러와, 상호작용 시도.
+                pickedUpObject = mapObjects.Find(obj => (obj.PosX == player.PosX) && (obj.PosY == player.PosY));
+                if (pickedUpObject is IInteractable)
+                {
+                    player.TryInteraction(pickedUpObject as IInteractable);
+                }
+                break;
+            case ConsoleKey.Escape:
+                // 메뉴 오픈
+                Menu.Open();
+                break;
+            default:
+                break;
+        }
+        // 플레이어 이동 방향의 오브젝트를 불러와, 그것이 벽이라면 이동 불가로 설정.
+        pickedUpObject = mapObjects.Find(obj => (obj.PosX == player.nextPosX) && (obj.PosY == player.nextPosY));
+        if (pickedUpObject is Wall)
+        {
+            player.Moveable = false;
+        }
+        player.Move();
+        // 플레이어와 겹친 오브젝트를 불러와, 자동 상호작용 시도.
+        pickedUpObject = mapObjects.Find(obj => (obj.PosX == player.PosX) && (obj.PosY == player.PosY));
+        if (pickedUpObject is IAutoInteractable)
+        {
+            player.TryInteraction(pickedUpObject as IAutoInteractable);
+            if (pickedUpObject.Gettable)
+            {
+                mapObjects.Remove(pickedUpObject);
+            }
+        }
+        pickedUpObject = null;
+    }
 }
